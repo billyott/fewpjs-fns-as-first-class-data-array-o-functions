aebe82bf30c06ce5dd37bacae169ca19ac7ca6c2
billyott <willrichott@gmail.com>
2020-10-25 21:17:26 -0400

    Done

---
+++ index.js
@@ -0,0 +1,50 @@
+function wakeDog(dogName, dogBreed) {
+    const string = `Wake ${dogName} the ${dogBreed}`;
+    // console.log(string)
+    return string
+}
+
+function leashDog(dogName, dogBreed) {
+    const string = `Leash ${dogName} the ${dogBreed}`;
+    // console.log(string)
+    return string
+}
+
+function walkToPark(dogName, dogBreed) {
+    const string = `Walk to the park with ${dogName} the ${dogBreed}`;
+    // console.log(string)
+    return string
+}
+
+function throwFrisbee(dogName, dogBreed) {
+    const string = `Throw the frisbee for ${dogName} the ${dogBreed}`;
+    // console.log(string)
+    return string
+}
+
+function walkHome(dogName, dogBreed) {
+    const string = `Walk home with ${dogName} the ${dogBreed}`;
+    // console.log(string)
+    return string
+}
+
+function unleashDog(dogName, dogBreed) {
+    const string = `Unleash ${dogName} the ${dogBreed}`;
+    // console.log(string)
+    return string
+}
+
+const routine = [wakeDog, leashDog, walkToPark, throwFrisbee, walkHome, unleashDog]
+
+function exerciseDog(dogName, dogBreed) {
+    const array = []
+    for (let i = 0; i < routine.length; i++) {
+        const output = routine[i](dogName, dogBreed)
+        // console.log(output)
+        array.push(output)
+    }
+    // console.log(array)
+    return array
+}
+
+console.log(exerciseDog("sheeby","shiba"))
